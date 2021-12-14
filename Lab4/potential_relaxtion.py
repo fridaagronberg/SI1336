b79b6010bc210070965adb93cb46f7856c497704
frigro <frigro@kth.se>
2021-12-14 12:12:44 +0100

    .

--- Lab4/potential_relaxtion.py
+++ Lab4/potential_relaxtion.py
@@ -3,15 +3,28 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 
+
 class LaplaceApp:
-    def __init__(self, length=10, steplength=1, left_boundary=10, right_boundary=10,
-                 top_boundary=10, bottom_boundary=10, inital_grid=None, method=_simple_relaxtion_iteration):
-        self.grid = np.zeros(length//steplength, length//steplength)
+    def __init__(self, expected_grid, length=10, steplength=1, left_boundary=10, right_boundary=10,
+                 top_boundary=10, bottom_boundary=10, initial_grid=None):
+        self.grid = np.zeros([length//steplength, length//steplength])
         self.length = length
         self.steplength = steplength
+        self.expected_grid = expected_grid
+        self.number_of_iterations = 0
+
         self._set_boundary(left_boundary, right_boundary, top_boundary, bottom_boundary)
-        self._set_initial_values(inital_grid)
-        self.method = method
+        self._set_initial_values(initial_grid)
+
+        # Sjukt obehagligt med det här är i init
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        self.im = ax.imshow(self.grid, cmap=None, interpolation='nearest')
+        fig.colorbar(self.im)
+
+        self.ani = animation.FuncAnimation(fig, self.calculate_potential, frames=60,
+                                            interval=200, blit=True, repeat = False)
+        plt.show()
 
     def _set_boundary(self, left_boundary, right_boundary, top_boundary, bottom_boundary):
         self.grid[:, 0] = left_boundary
@@ -20,32 +33,30 @@ class LaplaceApp:
         self.grid[-1, :] = bottom_boundary
 
     def _set_initial_values(self, inital_grid):
-        if inital_grid:
-            self.grid[1:-1, 1:-1] = inital_grid
-        else:
+        if inital_grid.any() == None:
             pass
+        else:
+            self.grid[1:-1, 1:-1] = inital_grid
 
     def _simple_relaxtion_iteration(self):
-        new_grid = np.zeros(self.length//self.steplength-1, self.length//self.steplength-1)
-        for i in range(1, self.length//self.steplength):
-            for j in range(1, self.length // self.steplength):
+        new_grid = np.zeros([self.length//self.steplength-2, self.length//self.steplength-2])
+        for i in range(1, self.length//self.steplength-1):
+            for j in range(1, self.length // self.steplength-1):
                 new_grid[i-1, j-1] = 0.25*(self.grid[i+self.steplength, j] + self.grid[i-self.steplength, j]
                                             + self.grid[i, j-self.steplength] + self.grid[i, j+self.steplength])
-        error = max(abs((new_grid - self.grid[1:-1, 1:-1]) / self.grid[1:-1, 1:-1]))
+        x = (new_grid - self.expected_grid) / self.expected_grid
+        error = abs(max(x.min(), x.max(), key=abs))
         self.grid[1:-1, 1:-1] = new_grid
         return error
 
-    def calculate_potential(self, max_error=0.01):
-        # Initialize animation?
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        im = ax.imshow(v, cmap=None, interpolation='nearest')
-        fig.colorbar(im)
-        number_of_iterations = 0
-        while True:
-            error = self.method()
-            number_of_iterations += 1
-            if error < max_error:
-                break
-        print('Number of iterations required ', number_of_iterations)
+    def calculate_potential(self, framedata):
+        error = self._simple_relaxtion_iteration()
+        self.number_of_iterations += 1
+        if error < 0.01:
+            self.ani.event_source.stop()
+            print('Number of iterations required ', self.number_of_iterations)
+        self.im.set_array(self.grid)
+        return self.im,
 
+initial_grid = 9*np.ones([8, 8])
+LaplaceApp(expected_grid=initial_grid+1, initial_grid=initial_grid)
